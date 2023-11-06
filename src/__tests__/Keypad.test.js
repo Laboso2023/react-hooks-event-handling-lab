30b16c56564df05e19ce7fed008a2d9bd018848c
Denis laboso <denis.laboso@student.moringaschool.com>
2023-11-06 10:24:54 +0300

    done

--- src/__tests__/Keypad.test.js
+++ src/__tests__/Keypad.test.js
@@ -1,30 +1,14 @@
-import "@testing-library/jest-dom";
-import { render, fireEvent } from "@testing-library/react";
-import Keypad from "../components/Keypad";
+import React from 'react'
 
-let container;
+function Keypad() {
 
-beforeEach(() => {
-  container = render(<Keypad />).container;
-});
+  function handleChange() {
+    console.log("Entering password...")
+  }
 
-test("displays one input", () => {
-  const input = container.querySelector("input");
-  expect(input).toBeInTheDocument();
-  expect(input.tagName).toBe("INPUT");
-});
+  return (
+    <input type="password" onChange={handleChange} />
+  )
+}
 
-test("displays an input with the right input type", () => {
-  const input = container.querySelector("input");
-  expect(input.type).toBe("password");
-});
-
-test("typing in the input triggers console output", () => {
-  console.log = jest.fn();
-
-  const input = container.querySelector("input");
-  fireEvent.change(input, { target: { value: "123" } });
-
-  expect(console.log).toHaveBeenCalled();
-  expect(console.log.mock.calls[0][0]).toBe("Entering password...");
-});
+export default Keypad
